84906fae1ad795dc9f03e40e4504ced709213afc
packt-joeld <joeld@packtpub.com>
2017-12-06 15:48:23 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,33 +1,12 @@
-###############################################################################
-# START HERE: Tutorial 1: Getting used to the ScraperWiki editing interface.
-# Follow the actions listed with -- BLOCK CAPITALS below.
-###############################################################################
-
-# -----------------------------------------------------------------------------
-# 1. Start by running a really simple Python script, just to make sure that 
-# everything is working OK.
-# -- CLICK THE 'RUN' BUTTON BELOW
-# You should see some numbers print in the 'Console' tab below. If it doesn't work, 
-# try reopening this page in a different browser - Chrome or the latest Firefox.
-# -----------------------------------------------------------------------------
-
-for i in range(10):
-    print "Hello", i
-
-# -----------------------------------------------------------------------------
-# 2. Next, try scraping an actual web page and getting some raw HTML.
-# -- UNCOMMENT THE THREE LINES BELOW (i.e. delete the # at the start of the lines)
-# -- CLICK THE 'RUN' BUTTON AGAIN 
-# You should see the raw HTML at the bottom of the 'Console' tab. 
-# Click on the 'more' link to see it all, and the 'Sources' tab to see our URL - 
-# you can click on the URL to see the original page. 
-# -----------------------------------------------------------------------------
-
-#import scraperwiki
-#html = scraperwiki.scrape('https://scraperwiki.com/hello_world.html')
-#print html
-
-# -----------------------------------------------------------------------------
-# In the next tutorial, you'll learn how to extract the useful parts
-# from the raw HTML page.
-# -----------------------------------------------------------------------------
+import scraperwiki
+html = scraperwiki.scrape('http://inmo.ie/6022')
+import lxml.html
+root = lxml.html.fromstring(html) # turn our HTML into an lxml object
+tds = root.cssselect('td') # get all the <td tags
+for td in tds:
+    record = { "td" : td.text } # column name and value
+    try:
+        scraperwiki.sqlite.save(["td"], record) # save the records one by one
+    except:
+        record = { "td" : "NO ENTRY" } # column name and value
+        scraperwiki.sqlite.save(["td"], record) # save the records one by one
